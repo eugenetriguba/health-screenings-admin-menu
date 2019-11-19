a0a6fcf21b7a0c40c11193f8cbdf72a2149eedd7
Eugene Triguba <eugenetriguba@gmail.com>
2019-11-18 22:20:52 -0600

    Remove imports from authorize calendar api unit test

--- tests/unit/authorize_calendar_api.spec.js
+++ tests/unit/authorize_calendar_api.spec.js
@@ -1,8 +1,5 @@
 import { expect } from "chai";
 import env from "env";
-import {
-    loadCredentials, authorizeCalendarApi, getAccessToken, listEvents
-} from "../../src/app/authorize_calendar_api";
 
 describe("authorizing calendar api", () => {
     // write tests here
