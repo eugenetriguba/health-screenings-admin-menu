31781cbb9a5bdfb12688b9a997b4360e656c58f3
Eugene Triguba <eugenetriguba@gmail.com>
2019-11-26 16:06:27 -0600

    Install sequelize and add test setup script

---
+++ app/database/setup.js
@@ -0,0 +1,15 @@
+// import Sequelize from "sequelize";
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: '.'
+});
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully!');
+    })
+    .catch(error => {
+        console.error('Unable to connect to the database: ', error);
+    });
