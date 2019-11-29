58b4972ca6cdb07ef0ca8d4ed467611b00fa70bb
Eugene Triguba <eugenetriguba@gmail.com>
2019-11-29 13:35:24 -0600

    Add booking and waitlist table

--- app/database/models/index.js
+++ app/database/models/index.js
@@ -2,12 +2,13 @@
 
 import fs from 'fs';
 import path from 'path';
+import sqlite from 'sqlite3';
 import Sequelize from 'sequelize';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config.json')[env];
-const db = {};
+const db = new sqlite.Database('..' + config.storage);
 
 let sequelize = new Sequelize({
     dialect: config.dialect,
